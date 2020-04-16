43bb26f8e55057b240c8d03e6f76f46404d1e997
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2020-04-16 17:13:22 +0300

    Added required models

---
+++ models.go
@@ -0,0 +1,30 @@
+package gofer
+
+// Pair 
+type Pair struct {
+	Base string,
+	Quote string
+}
+
+// Exchange
+type Exchange struct {
+	Name string
+	Config map[string]string
+}
+
+// PotentialPricePoint
+type PotentialPricePoint struct {
+	Pair *Pair,
+	Exchange *Exchange
+}
+
+// PricePoint
+type PricePoint struct {
+	Exchange *Exchange,
+	Pair *Pair
+	Timestamp int64,
+	Price float64,
+	Bid float64,
+	Ask float64,
+	Volume float64
+}
