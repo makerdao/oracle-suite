603a5c9818ac09c0f8eb32134dafa52d632b37aa
Christopher Fredén <me@icetan.org>
2020-04-28 18:34:59 +0200

    Update license header in Go source files

--- exchange/errors.go
+++ exchange/errors.go
@@ -1,4 +1,4 @@
-//  Copyright (C) 2020  Maker Foundation
+//  Copyright (C) 2020 Maker Ecosystem Growth Holdings, INC.
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Affero General Public License as
