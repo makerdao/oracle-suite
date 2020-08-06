594e4fb5b064edb347b503d43a14dfd9906b2158
Michal Dobaczewski <mdobak@gmail.com>
2020-08-06 13:27:58 +0200

    Create gofer exchange implementation for [ch4848]

--- exchange/exchanges.go
+++ exchange/exchanges.go
@@ -52,6 +52,7 @@ var exchangeList = map[string]Handler{
 	"upbit":       &Upbit{},
 	"folgory":     &Folgory{},
 	"kraken":      &Kraken{},
+	"okex":        &Okex{},
 	"uniswap":     &Uniswap{},
 }
 
