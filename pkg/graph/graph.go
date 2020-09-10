370156af96676adda668563d3f894ec1f3a52698
Michal Dobaczewski <mdobak@gmail.com>
2020-09-11 01:19:01 +0200

    Fix walk method, walk only on unique nodes

--- pkg/graph/graph.go
+++ pkg/graph/graph.go
@@ -24,30 +24,32 @@ type Exchange interface {
 }
 
 func Walk(node Node, fn func(Node)) {
-	for _, n := range node.Children() {
-		Walk(n, fn)
-	}
+	nodes := map[Node]struct{}{}
 
-	fn(node)
+	var recur func(Node)
+	recur = func(node Node) {
+		nodes[node] = struct{}{}
+		for _, n := range node.Children() {
+			recur(n)
+		}
+	}
+	recur(node)
+
+	for n, _ := range nodes {
+		fn(n)
+	}
 }
 
 func AsyncWalk(node Node, fn func(Node)) {
 	wg := sync.WaitGroup{}
 
-	nodes := map[Node]struct{}{}
 	Walk(node, func(node Node) {
-		nodes[node] = struct{}{}
-	})
-
-	for n, _ := range nodes {
-		n := n
 		wg.Add(1)
 		go func() {
-			fn(n)
+			fn(node)
 			wg.Done()
 		}()
-	}
+	})
 
-	fn(node)
 	wg.Wait()
 }
