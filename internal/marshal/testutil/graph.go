f2844f67ea96619afbb82837d0eba4e20907606c
Michal Dobaczewski <mdobak@gmail.com>
2020-10-06 02:51:24 +0200

    Add license header

--- internal/marshal/testutil/graph.go
+++ internal/marshal/testutil/graph.go
@@ -1,3 +1,18 @@
+//  Copyright (C) 2020 Maker Ecosystem Growth Holdings, INC.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package testutil
 
 import (
@@ -24,7 +39,7 @@ func Graph(p graph.Pair) *graph.MedianAggregatorNode {
 	mn.AddChild(on2)
 
 	on1.Ingest(graph.OriginTick{
-		Tick:   graph.Tick{
+		Tick: graph.Tick{
 			Pair:      p,
 			Price:     10,
 			Bid:       10,
@@ -37,7 +52,7 @@ func Graph(p graph.Pair) *graph.MedianAggregatorNode {
 	})
 
 	on2.Ingest(graph.OriginTick{
-		Tick:   graph.Tick{
+		Tick: graph.Tick{
 			Pair:      p,
 			Price:     20,
 			Bid:       20,
