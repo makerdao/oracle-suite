0c279553c7e30d9572d329f580726c6cb393abeb
Paweł Zaremba <pawzar@gmail.com>
2020-10-20 14:51:23 +0200

    Add relayer pkg

--- pkg/relayer/relayer.go
+++ pkg/relayer/relayer.go
@@ -13,13 +13,4 @@
 //  You should have received a copy of the GNU Affero General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-package main
-
-import "github.com/hashicorp/serf/cmd/serf/command/agent"
-
-func main() {
-	&agent.Command{
-		Ui:         ui,
-		ShutdownCh: make(chan struct{}),
-	}
-}
+package relayer
