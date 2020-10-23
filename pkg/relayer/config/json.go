76df58c9a859ef8a7fcfd4b7aa4b6a055b0cc85f
Paweł Zaremba <pawzar@gmail.com>
2020-10-23 18:32:07 +0200

    Copyright

--- pkg/relayer/config/json.go
+++ pkg/relayer/config/json.go
@@ -1,3 +1,18 @@
+//  Copyright (C) 2020 Maker Ecosystem Growth Holdings, INC.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package config
 
 import (
@@ -102,7 +117,7 @@ func (j *JSON) MakeRelayer() (*relayer.Relayer, error) {
 	}
 
 	eth := ethereum.NewClient(client, wallet)
-	rel := relayer.NewRelayer(transport, time.Second * time.Duration(j.Options.Interval))
+	rel := relayer.NewRelayer(transport, time.Second*time.Duration(j.Options.Interval))
 
 	for name, pair := range j.Pairs {
 		rel.AddPair(relayer.Pair{
