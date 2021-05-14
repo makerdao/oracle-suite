20cc6c67062a3d7f169cd7a0759f53badf1db47f
Paweł Zaremba <pawzar@gmail.com>
2021-05-14 20:27:14 +0200

    Split spire & gofer derivations

--- default.nix
+++ default.nix
@@ -1,9 +1,20 @@
-{ buildGoModule
-}: buildGoModule rec {
-  pname = "oracle-suite";
-  version = "dev";
-  src = ./.;
-  vendorSha256 = "07fzxllv6zbh38z28l6pi0iv1ynndrcb6lb77hv26ilprx3gzfvm";
-  subPackages = [ "cmd/gofer" "cmd/spire" ];
-  postConfigure = "export CGO_ENABLED=0";
+{ buildGoModule }: {
+  gofer = buildGoModule {
+    pname = "gofer";
+    version = "dev";
+    src = ./.;
+    vendorSha256 = "07fzxllv6zbh38z28l6pi0iv1ynndrcb6lb77hv26ilprx3gzfvm";
+    subPackages = [ "cmd/gofer" ];
+    postConfigure = "export CGO_ENABLED=0";
+    postInstall = "cp ./gofer.json $out";
+  };
+  spire = buildGoModule {
+    pname = "spire";
+    version = "dev";
+    src = ./.;
+    vendorSha256 = "07fzxllv6zbh38z28l6pi0iv1ynndrcb6lb77hv26ilprx3gzfvm";
+    subPackages = [ "cmd/spire" ];
+    postConfigure = "export CGO_ENABLED=0";
+    postInstall = "cp ./spire.json $out";
+  };
 }
--- shell.nix
+++ shell.nix
@@ -1,5 +1,2 @@
-{ pkgs ? import <nixpkgs> {}
-, oracle-suite ? pkgs.callPackage  ./default.nix {}
-}: pkgs.mkShell {
-  buildInputs = [ oracle-suite ];
-}
+{ pkgs ? import <nixpkgs> { }, oracle-suite ? pkgs.callPackage ./default.nix { } }:
+pkgs.mkShell { buildInputs = [ oracle-suite.gofer oracle-suite.spire ]; }
