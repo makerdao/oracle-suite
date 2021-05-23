b869b936c26b99f2315d905781e560ce04ffc51f
Paweł Zaremba <pawzar@gmail.com>
2021-05-23 23:53:14 +0200

    Update version

--- default.nix
+++ default.nix
@@ -1,7 +1,18 @@
-{ buildGoModule ? (import <nixpkgs> { }).buildGo116Module }:
-buildGoModule {
+{ pkgs ? import <nixpkgs> { }, buildGoModule ? pkgs.buildGo116Module }:
+let
+  rev = pkgs.stdenv.mkDerivation {
+    name = "rev";
+    buildInputs = [ pkgs.git ];
+    src = ./.;
+    buildPhase = "true";
+    installPhase = ''
+      echo "$(git rev-parse --short HEAD 2>/dev/null || find . -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum | sed -r 's/\s+//g')" > $out
+    '';
+  };
+  ver = "${pkgs.lib.removeSuffix "\n" (builtins.readFile "${rev}")}";
+in buildGoModule {
   pname = "oracle-suite";
-  version = "dev20210521";
+  version = "dev_${ver}";
   src = ./.;
   vendorSha256 = "11xjwx8gn2iwca66783fmddnjs3z5pnn0rk87lpbpas9p6pipakj";
   subPackages = [ "cmd/..." ];
