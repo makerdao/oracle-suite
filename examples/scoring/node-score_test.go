85137e411d4daa2498c1ab1832244801019f3fbd
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2021-06-06 12:10:54 +0300

    Added spam invalid messages test

--- examples/scoring/node-score_test.go
+++ examples/scoring/node-score_test.go
@@ -3,6 +3,8 @@ package scoring
 import (
 	"context"
 	"math/rand"
+	"strconv"
+	"sync"
 	"testing"
 	"time"
 
@@ -12,9 +14,9 @@ import (
 
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/protocol"
 
-	logging "github.com/ipfs/go-log"
 	pb "github.com/libp2p/go-libp2p-pubsub/pb"
 	"github.com/libp2p/go-msgio/protoio"
 )
@@ -22,7 +24,6 @@ import (
 const baseHostAddr = "/ip4/0.0.0.0/tcp/0"
 
 func startNode(t *testing.T, ctx context.Context) host.Host {
-	//p := tnet.RandPeerNetParamsOrFatal(t)
 	// create a new libp2p Host that listens on a random TCP port
 	h, err := libp2p.New(ctx, libp2p.ListenAddrStrings(baseHostAddr))
 	if err != nil {
@@ -168,6 +169,140 @@ func TestGossipsubAttackSpamIWANT(t *testing.T) {
 	<-ctx.Done()
 }
 
+// Test that when Gossipsub receives a lot of invalid messages from
+// a peer it should graylist the peer
+func TestGossipsubAttackInvalidMessageSpam(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Create legitimate and attacker hosts
+	legit := startNode(t, ctx)
+	attacker := startNode(t, ctx)
+
+	mytopic := "mytopic"
+
+	// Create parameters with reasonable default values
+	params := &pubsub.PeerScoreParams{
+		AppSpecificScore:            func(peer.ID) float64 { return 0 },
+		IPColocationFactorWeight:    0,
+		IPColocationFactorThreshold: 1,
+		DecayInterval:               5 * time.Second,
+		DecayToZero:                 0.01,
+		RetainScore:                 10 * time.Second,
+		Topics:                      make(map[string]*pubsub.TopicScoreParams),
+	}
+	params.Topics[mytopic] = &pubsub.TopicScoreParams{
+		TopicWeight:                     0.25,
+		TimeInMeshWeight:                0.0027,
+		TimeInMeshQuantum:               time.Second,
+		TimeInMeshCap:                   3600,
+		FirstMessageDeliveriesWeight:    0.664,
+		FirstMessageDeliveriesDecay:     0.9916,
+		FirstMessageDeliveriesCap:       1500,
+		MeshMessageDeliveriesWeight:     -0.25,
+		MeshMessageDeliveriesDecay:      0.97,
+		MeshMessageDeliveriesCap:        400,
+		MeshMessageDeliveriesThreshold:  100,
+		MeshMessageDeliveriesActivation: 30 * time.Second,
+		MeshMessageDeliveriesWindow:     5 * time.Minute,
+		MeshFailurePenaltyWeight:        -0.25,
+		MeshFailurePenaltyDecay:         0.997,
+		InvalidMessageDeliveriesWeight:  -99,
+		InvalidMessageDeliveriesDecay:   0.9994,
+	}
+	thresholds := &pubsub.PeerScoreThresholds{
+		GossipThreshold:   -100,
+		PublishThreshold:  -200,
+		GraylistThreshold: -300,
+		AcceptPXThreshold: 0,
+	}
+
+	// Set up gossipsub on the legit host
+	tracer := &gsAttackInvalidMsgTracer{}
+	ps, err := pubsub.NewGossipSub(ctx, legit,
+		pubsub.WithEventTracer(tracer),
+		pubsub.WithPeerScore(params, thresholds),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	//attackerScore := func() float64 {
+	//	return ps.rt.(*GossipSubRouter).score.Score(attacker.ID())
+	//}
+
+	// Subscribe to mytopic on the legit host
+	_, err = ps.Subscribe(mytopic)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pruneCount := 0
+	pruneCountMx := sync.Mutex{}
+	getPruneCount := func() int {
+		pruneCountMx.Lock()
+		defer pruneCountMx.Unlock()
+		return pruneCount
+	}
+	addPruneCount := func(i int) {
+		pruneCountMx.Lock()
+		defer pruneCountMx.Unlock()
+		pruneCount += i
+	}
+
+	newMockGS(ctx, t, attacker, func(writeMsg func(*pb.RPC), irpc *pb.RPC) {
+		// When the legit host connects it will send us its subscriptions
+		for _, sub := range irpc.GetSubscriptions() {
+			if sub.GetSubscribe() {
+				// Reply by subcribing to the topic and grafting to the peer
+				writeMsg(&pb.RPC{
+					Subscriptions: []*pb.RPC_SubOpts{&pb.RPC_SubOpts{Subscribe: sub.Subscribe, Topicid: sub.Topicid}},
+					Control:       &pb.ControlMessage{Graft: []*pb.ControlGraft{&pb.ControlGraft{TopicID: sub.Topicid}}},
+				})
+
+				go func() {
+					defer cancel()
+
+					// Send a bunch of messages with no signature (these will
+					// fail validation and reduce the attacker's score)
+					for i := 0; i < 100; i++ {
+						msg := &pb.Message{
+							Data:  []byte("some data" + strconv.Itoa(i)),
+							Topic: &mytopic,
+							From:  []byte(attacker.ID()),
+							Seqno: []byte{byte(i + 1)},
+						}
+						writeMsg(&pb.RPC{
+							Publish: []*pb.Message{msg},
+						})
+					}
+
+					// Wait for the initial heartbeat, plus a bit of padding
+					time.Sleep(100*time.Millisecond + pubsub.GossipSubHeartbeatInitialDelay)
+
+					// There should be several rejected messages (because the signature was invalid)
+					if tracer.rejectCount == 0 {
+						t.Fatal("Expected message rejection but got none")
+					}
+					// The legit node should have sent a PRUNE message
+					pc := getPruneCount()
+					if pc == 0 {
+						t.Fatal("Expected attacker node to be PRUNED when score drops low enough")
+					}
+				}()
+			}
+		}
+
+		if ctl := irpc.GetControl(); ctl != nil {
+			addPruneCount(len(ctl.GetPrune()))
+		}
+	})
+
+	connect(t, legit, attacker)
+
+	<-ctx.Done()
+}
+
 type gsAttackInvalidMsgTracer struct {
 	rejectCount int
 }
@@ -177,10 +312,6 @@ func (t *gsAttackInvalidMsgTracer) Trace(evt *pb.TraceEvent) {
 	if evt.GetType() == pb.TraceEvent_REJECT_MESSAGE {
 		t.rejectCount++
 	}
-}
-
-func turnOnPubsubDebug() {
-	logging.SetLogLevel("pubsub", "debug")
 }
 
 type mockGSOnRead func(writeMsg func(*pb.RPC), irpc *pb.RPC)
