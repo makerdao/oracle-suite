a0ebd9d4bce4a4b8c91626830357123a15789943
Paweł Zaremba <pawzar@gmail.com>
2021-06-11 17:49:58 +0200

    Revert nix build config

--- default.nix
+++ default.nix
@@ -1,7 +1,18 @@
 { pkgs ? import <nixpkgs> { }, buildGoModule ? pkgs.buildGo116Module }:
-buildGoModule {
+let
+  rev = pkgs.stdenv.mkDerivation {
+    name = "rev";
+    buildInputs = [ pkgs.git ];
+    src = ./.;
+    buildPhase = "true";
+    installPhase = ''
+      echo "$(git rev-parse --short HEAD 2>/dev/null || find * -type f -name '*.go' -print0 | sort -z | xargs -0 sha1sum | sha1sum | sed -r 's/[^\da-f]+//g')" > $out
+    '';
+  };
+  ver = "${pkgs.lib.removeSuffix "\n" (builtins.readFile "${rev}")}";
+in buildGoModule {
   pname = "oracle-suite";
-  version = pkgs.lib.fileContents ./version;
+  version = "dev_${ver}";
   src = ./.;
   vendorSha256 = "13g3s50kzaksbd64zsgxsxlyxqz5agkapp6iq0yf4darzj6fd7ny";
   subPackages = [ "cmd/..." ];
