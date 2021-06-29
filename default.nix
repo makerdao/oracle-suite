499ec1ec9c6f967d732d63123c8a27db63484af0
Paweł Zaremba <pawzar@gmail.com>
2021-06-29 21:49:46 +0200

    Prepare v0.1.4

--- default.nix
+++ default.nix
@@ -15,7 +15,7 @@ in buildGoModule {
   #  version = "dev-${ver}";
   version = pkgs.lib.fileContents ./version;
   src = ./.;
-  vendorSha256 = "15hlsx81kpwly7wdvaz2kcqksvkys041v3fg3jrp20ya5xyxg83g";
+  vendorSha256 = "0ix8ipdjx5656rjl0bbd27kdhia4mynl7jikqsnlddb554wvndpd";
   subPackages = [ "cmd/..." ];
   postConfigure = "export CGO_ENABLED=0";
   postInstall = "cp ./config.json $out";
