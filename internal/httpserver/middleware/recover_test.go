87bc5403394f8328aa30b80395f6e23006c19e10
Michal Dobaczewski <mdobak@gmail.com>
2021-09-24 06:41:28 +0200

    Update httpserver test

--- internal/httpserver/middleware/recover_test.go
+++ internal/httpserver/middleware/recover_test.go
@@ -23,31 +23,30 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRecover(t *testing.T) {
-	t.Run("without-callback", func(t *testing.T) {
-		defer func() {
-			assert.Nil(t, recover())
-		}()
-		h := (&Recover{}).Handle(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-			panic("panic")
-		}))
-		r := httptest.NewRequest("GET", "/", nil)
-		rw := httptest.NewRecorder()
-		h.ServeHTTP(rw, r)
-	})
-	t.Run("with-callback", func(t *testing.T) {
-		var panicVal interface{}
-		defer func() {
-			assert.Nil(t, recover())
-			assert.Equal(t, panicVal, "panic")
-		}()
-		h := (&Recover{Recover: func(err interface{}) {
-			panicVal = err
-		}}).Handle(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-			panic("panic")
-		}))
-		r := httptest.NewRequest("GET", "/", nil)
-		rw := httptest.NewRecorder()
-		h.ServeHTTP(rw, r)
-	})
+func TestRecover_WithCallback(t *testing.T) {
+	var panicVal interface{}
+	defer func() {
+		assert.Nil(t, recover())
+		assert.Equal(t, panicVal, "panic")
+	}()
+	h := (&Recover{Recover: func(err interface{}) {
+		panicVal = err
+	}}).Handle(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		panic("panic")
+	}))
+	r := httptest.NewRequest("GET", "/", nil)
+	rw := httptest.NewRecorder()
+	h.ServeHTTP(rw, r)
+}
+
+func TestRecover_WithoutCallback(t *testing.T) {
+	defer func() {
+		assert.Nil(t, recover())
+	}()
+	h := (&Recover{}).Handle(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		panic("panic")
+	}))
+	r := httptest.NewRequest("GET", "/", nil)
+	rw := httptest.NewRecorder()
+	h.ServeHTTP(rw, r)
 }
